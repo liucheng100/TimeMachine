5e66952b187b6096175b290d09e962e4a673f0f1
SakuraKkkkk <526080338@qq.com>
2023-04-13 23:08:36 +0800

    我的作品页面

--- src/views/myWork.vue
+++ src/views/myWork.vue
@@ -1,59 +1,58 @@
 <template>
     <div class="myWork">
         <div class="top">
-            <div class="title">我的投稿</div>
+            <div class="title-main">我的投稿</div>
             <div class="flex">
                 <div class="auth">{{ auth }}</div>
                 <div class="avatar" :style="{backgroundImage: `url('${avatar}')`}"></div>
             </div>
         </div>
-        <div class="card-mini">
-            <div class="cover" :style="{ backgroundImage: `url('${cover}')` }"></div>
-            <div class="bot">
-                <div class="title">
-                    {{ title }}
-                </div>
-                <div class="bar">
-                    <div class="flex">
-                        <div class="avatar" :style="{ backgroundImage: `url('${avatar}')` }"></div>
-                        <div class="info">{{ auth }}</div>
+        <div class="cards">
+            <div class="card-work" v-for="(item,i) in works">
+                <div class="cover" :style="{ backgroundImage: `url('${item.cover}')` }"></div>
+                <div class="bot">
+                    <div class="title">
+                        {{ item.title }}
                     </div>
-                    <div class="flex">
-                        <div class="eye"></div>
-                        <div class="info">{{ hot }}</div>
+                    <div class="bar">
+                        <div class="time">{{time(i)}}</div>
+                        <div class="flex">
+                            <div class="eye"></div>
+                            <div class="info">{{ item.view }}</div>
+                        </div>
+                    </div>
+                    <div class="bar" v-if="!item.prizeId">
+                        <div class="status">{{pass(i)}}</div>
+                        <div class="flex">
+                            <div class="del"></div>
+                            <div class="info">删除</div>
+                        </div>
+                    </div>
+                    <div class="bar" v-if="item.prizeId">
+                        <div class="status_prized">{{prize(i)}}</div>
+                        <div class="ques">填写收货信息</div>
+
                     </div>
                 </div>
             </div>
         </div>
-        <!-- <div class="comps">
-            <div class="compCard" v-for="(item,index) in comps">
-                <div class="compPic" :style="{backgroundImage:'url('+NewURL(item.bannerPic)+')'}"></div>
-                <div class="compBottom">
-                    <div class="campTitles">
-                        <div class="campTitle">{{item.title}}</div>
-                        <div class="titleBottom">
-                            <div class="campSubTitle">{{item.subtitle}}</div>
-                            <div class="campStatus">{{status(index)}}</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> -->
     </div>
 </template>
 
 <script>
+    import { getPrize, } from '@/api/prize'
     export default {
-        name: "myWork",
         props: {
         },
         data() {
             return {
-                comps: [
-                    { title: "我的作品", status: 1, Pic: "../../assets/admin/testpic.jpg", Time: "2023-08-24T14:15:22" },
-                    { title: "我的作品", status: 2, Pic: "../../assets/admin/testpic.jpg", Time: "2023-05-24T14:15:22" },
-                    { title: "我的作品", status: 3, Pic: "../../assets/admin/testpic.jpg", Time: "2021-08-24T14:15:22" },
-                    { title: "我的作品", status: 4, Pic: "../../assets/admin/testpic.jpg", Time: "2019-08-24T14:15:22" },
+                name: "myWork",
+                avatar: "",
+                works: [
+                    { title: "我的作品", isPass: 0, prizeId: 0, Pic: "../../assets/admin/testpic.jpg", Time: "2023-08-24T14:15:22", view: 2123 },
+                    { title: "我的作品", isPass: 1, prizeId: 0, Pic: "../../assets/admin/testpic.jpg", Time: "2023-05-24T14:15:22", view: 2123 },
+                    { title: "我的作品", isPass: 2, prizeId: 0, Pic: "../../assets/admin/testpic.jpg", Time: "2021-08-24T14:15:22", view: 2123 },
+                    { title: "我的作品", isPass: 2, prizeId: 1, Pic: "../../assets/admin/testpic.jpg", Time: "2019-08-24T14:15:22", view: 2123 },
                 ],
                 auth: "aaasajsh"
             }
@@ -68,23 +67,24 @@
             setQues() {
                 this.$router.push("/admin/questionnaire")
             },
-            status(i) {
-                if (this.comps[i].status === 1) {
-                    let time = this.comps[i].startTime
-                    let t = new Date(time)
-                    let today = new Date();
-                    let day = parseInt((t - today) / 86400000)
-                    return "还有" + day + "天开始 · 未开始"
+            time(i) {
+                let time = this.works[i].Time
+                let t = new Date(time)
+
+                return t.getFullYear() + "/" + (t.getMonth() + 1) + "/" + t.getDate()
+            },
+            pass(i) {
+                if (this.works[i].isPass == 0) { return "未审核" }
+                else if (this.works[i].isPass == 1) { return "已通过" }
+                else if (this.works[i].isPass == 2) { return "未通过" }
+            },
+            prize(i) {
+                if (this.works[i].prizeId == 0) { return "未获奖" }
+                else {
+                    getPrize(this.works[i].prizeId).then(w => {
+                        return w.data.prizeName
+                    })
                 }
-                else if (this.comps[i].status === 2) {
-                    let time = this.comps[i].stopTime
-                    let t = new Date(time)
-                    let today = new Date();
-                    let day = parseInt((t - today) / 86400000)
-                    return "还有" + day + "天截止 · 征稿中"
-                }
-                else if (this.comps[i].status === 3) { return "评审中" }
-                else if (this.comps[i].status === 4) { return "已结束" }
             }
         },
         mounted() {
@@ -107,7 +107,7 @@
         padding-right: 20px;
     }
 
-    .title {
+    .title-main {
         margin-left: 20px;
         margin-top: 20px;
         font-size: 32px;
@@ -120,7 +120,7 @@
     .auth {
         font-size: 14px;
         height: 20px;
-        color: rgba(153, 156, 160, 1);
+        color: #999CA0;
         transform: translateY(-.6px);
     }
 
@@ -138,5 +138,104 @@
         box-shadow: 0px 2px 15px rgba(0, 0, 0, 0.1);
         background: center center / 100% no-repeat;
         background-color: rgba(217, 217, 217, 1);
+    }
+
+    .cards {
+        width: 100%;
+        min-height: 300px;
+        padding: 10px 20px;
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        background-color: #fff;
+        margin-bottom: 100px;
+
+    }
+
+    .card-work {
+        width: calc(50vw - 30px);
+        max-width: 43vw;
+        min-width: 40vw;
+        margin-bottom: 20px;
+        height: 252px;
+        display: flex;
+        flex-direction: column;
+        border-radius: 6px;
+        overflow: hidden;
+        background-color: #fff;
+        box-shadow: 0px 2px 30px rgba(0, 0, 0, 0.1);
+    }
+
+    .cover {
+        height: 166px;
+        background: center center / cover no-repeat;
+        background-color: #53517742;
+    }
+
+    .bot {
+        height: 64px;
+        padding: 10px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    .time {
+        height: 17px;
+        font-size: 12px;
+        color: #999CA0;
+    }
+
+    .title {
+        font-size: 16px;
+        color: rgba(31, 31, 31, 1);
+    }
+
+    .bar {
+        margin-top: 5px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .eye {
+        height: 13px;
+        width: 13px;
+        background: url('../assets/eye.svg') center center / 100% no-repeat;
+        margin-right: 5px;
+    }
+
+    .del {
+        height: 13px;
+        width: 13px;
+        background: url('../assets/del.svg') center center / 100% no-repeat;
+        margin-right: 5px;
+    }
+
+    .info {
+        font-size: 12px;
+        font-weight: 300;
+        color: #999CA0;
+        transform: translateY(-.6px);
+    }
+
+    .status {
+        font-size: 12px;
+        font-weight: 300;
+        color: #999CA0;
+    }
+
+    .status_prized {
+        font-size: 12px;
+        font-weight: 300;
+        color: #4E46B4;
+    }
+
+
+    .ques {
+        font-size: 12px;
+        font-weight: 300;
+        color: #4E46B4;
+        transform: translateY(-.6px);
     }
 </style>
