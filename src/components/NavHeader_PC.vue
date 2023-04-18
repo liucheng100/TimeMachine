3876b647352804ad0b4cb8d004e1b6b07a55ca54
Jerry <1422768678@qq.com>
2023-04-18 12:30:43 +0800

    退出登录修复

--- src/components/NavHeader_PC.vue
+++ src/components/NavHeader_PC.vue
@@ -5,7 +5,10 @@
             <p class="go-to-phone">前往手机端浏览作品</p>
             <p v-show="!is_login" class="login-text" @click="is_need_login = !is_need_login">登录</p>
             <p v-show="is_login" class="personal-center">个人中心</p>
-            <p v-show="is_login" class="personal-center" @click="logOut()">退出登录</p>
+            <p v-show="is_login" class="username" @click="logOut()" 
+            @mouseenter="usernameOld = username, username = '退出登录'" 
+            @mouseleave="username = usernameOld"
+            >{{ username }}</p>
         </div>
         <div class="mask" v-show="is_need_login" @click="changeNeedLogin()">
             <LoginPC @tokenGet="is_login = true"/>
@@ -15,11 +18,20 @@
 
 <script setup>
 import LoginPC from "@/components/PC/LoginPC.vue"
+import { getToken, getUserId } from "@/utils/auth";
 import { ref } from "vue"
 import { removeToken } from "@/utils/auth";
-
+import { getUserInfo } from "@/api/login"
+const username = ref("");
+const usernameOld = "";
 const is_login = ref(false);
 const is_need_login = ref(false);
+
+if(getToken()){
+    getUserInfo(getUserId())
+    .then(res => username.value = res.data.username);
+}
+
 
 function changeLogin(){
     is_login.value = !is_login.value;
@@ -59,9 +71,16 @@ function logOut(){
     display: flex;
     font-size:4px;
     color:#FFFFFF;
+    width:25%;
+    justify-content: flex-end;
 }
 .login-text, .go-to-phone, .personal-center{
     margin-right:8px;
+    cursor:pointer;
+}
+
+.username{
+    width:20%;
     cursor:pointer;
 }
 </style>
