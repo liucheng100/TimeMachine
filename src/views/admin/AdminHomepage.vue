4bdd063b5dd125fac75103cbf4f853a557965baa
SakuraKkkkk <526080338@qq.com>
2023-04-18 21:33:50 +0800

    tabmagic props

--- src/views/admin/AdminHomepage.vue
+++ src/views/admin/AdminHomepage.vue
@@ -1,21 +1,17 @@
-<!-- <template>
-    <div class="homepage" @scroll="scroll">
-        <div class="banner">
-            <img class="banner-img" src="../../assets/warnCircle.svg" alt="">
-            <div class="banner-tip">还有24天截稿</div>
-        </div>
+<template>
+    <div id="homepage" ref="homepage" class="homepage" @scroll="scroll">
         <div class="info">
             <div class="info-left">
-                <div class="info-title">觅·新</div>
-                <div class="info-tip">第十三届方寸·流年摄影大赛</div>
+                <div class="info-title">{{ contest.title }}</div>
+                <div class="info-tip">{{ contest.subtitle }}</div>
             </div>
-            <SeasonBtn @click.native="!seasonState?ON=true:0" :state="seasonState"></SeasonBtn>
+            <SeasonBtn @click.native="contest.status==2 ? ON = true : 0" :state="contest.status-1 || 0"></SeasonBtn>
         </div>
-        <TabMagic :id="1" sticky="0" :title_list="['赛事介绍','全部作品','获奖作品']" @tab0Click="seasonState=0"
-            @tab1Click="seasonState=1" @tab2Click="seasonState=2">
+        <TabMagic :id="11451" sticky="0" :title_list="['赛事介绍', '全部作品', '获奖作品']" @tab0Click="seasonState = 0"
+            :activeIdx="1" @tab1Click="seasonState = 1" @tab2Click="seasonState = 2">
             <template v-slot:tab0>
                 <div class="tab-0">
-                    <Page1></Page1>
+                    <Page1 :contest="contest"></Page1>
                 </div>
             </template>
             <template v-slot:tab1>
@@ -29,9 +25,13 @@
                 </div>
             </template>
         </TabMagic>
-        <Pop :ON="ON" :model="0" title="我们更推荐在PC端投稿作品" tip="以避免在手机上找不到文件。" :options="{black:'',grey:'坚持投稿',blue:'好的'}"
-            @blackClick="0" @greyClick="$router.push('/submit')" @blueClick="ON=false">
+        <Pop :ON="ON" :model="0" title="我们更推荐在PC端投稿作品" tip="以避免在手机上找不到文件。"
+            :options="{ black: '', grey: '坚持投稿', blue: '好的' }" @blackClick="0" @greyClick="$router.push({
+                path: '/submit',
+                query: { contestId: globalData.contestId }
+            }); ON = false" @blueClick="ON = false">
         </Pop>
+        <div :class="isSafari ? 'footer' : ''"></div>
     </div>
 </template>
 
@@ -39,16 +39,40 @@
     import Page1 from '@/components/homepage/Page1.vue'
     import Page2 from '@/components/homepage/Page2.vue'
     import Page3 from '@/components/homepage/Page3.vue'
+    import { getSrc, uploadFile, } from '@/api/file'
+    import { contesting, } from '@/api/contest'
+    import { isSafari } from '@/utils/common'
+    import pubuse from '@/utils/pub-use'
     export default {
+        name: 'homepage',
         components: [
             Page1, Page2, Page3
         ],
+        inject: ['globalData'],
         data() {
             return {
                 testList: [],
                 ON: false,
                 seasonState: 0,
                 byEnd: false,
+                catchTop: 0,
+                isSafari: true,
+                contest: {},
+            }
+        },
+        computed: {
+            restday() {
+                // stopTime: "1997-05-09T15:56:05"
+                let stopTime = this.contest.stopTime
+                let stopDate = new Date(stopTime);
+                let now = new Date();
+                let delta = stopDate - now; // 剩余毫秒数
+                let secondsPerDay = 1000 * 60 * 60 * 24; // 每天有多少毫秒
+                let daysLeft = Math.ceil(delta / secondsPerDay); // 向上取整，保证不误差
+                //console.log(daysLeft); // 输出距离截止时间还有多少天
+
+
+                return daysLeft
             }
         },
         methods: {
@@ -61,19 +85,67 @@
             scroll(e) {
                 // console.log(e)
                 let a = e.target.scrollTop
+                this.catchTop = a
                 let b = e.target.offsetHeight
                 let c = e.target.scrollHeight
                 let d = c - b - a
-                if (d < 50) {
+                // console.log(d)
+                if (d < 140) {
                     this.byEnd = true
                 } else {
                     this.byEnd = false
                 }
+            },
+
+            replaceBlob(tarObject, attrList) {
+                attrList.forEach(attr => {
+                    getSrc(tarObject[attr]).then(v => {
+                        // console.log(v)
+                        tarObject[attr] = v
+                    }).catch(err => {
+                        ElMessage.error('图片加载失败')
+                    })
+                    tarObject[attr] = pubuse('loading.gif')
+                });
             }
         },
         mounted() {
+            this.isSafari = isSafari()
+            contesting().then(v => {
+                console.log(118, v)
+                if (!v.code) {
+                    this.contest = v.data
+                    console.log(this.contest)
+                    this.replaceBlob(this.contest, [
+                        'bannerPic',
+                        'introductionPic',
+                        'tailPic',
+                    ])
+                    // prizes
+                    this.contest.prizes['1'].forEach(ele => {
+                        this.replaceBlob(ele, ['goodPic',])
+                    });
+                    this.contest.prizes['2'].forEach(ele => {
+                        this.replaceBlob(ele, ['goodPic',])
+                    });
+                    this.contest.prizes['3'].forEach(ele => {
+                        this.replaceBlob(ele, ['goodPic',])
+                    });
+                    this.contest.prizes['4'].forEach(ele => {
+                        this.replaceBlob(ele, ['goodPic',])
+                    });
+                    this.globalData.contestId = this.contest.contestId
+                    this.globalData.prizes = this.contest.prizes
+                } else {
+                    ElMessage.error(v.msg)
+                }
+            })
 
-
+        },
+        activated() {
+            // 对路由变化做出响应...
+            // alert(this.catchTop)
+            this.$refs.homepage.scrollTop = this.catchTop
         },
 
     }
@@ -81,35 +153,11 @@
 
 
 <style scoped>
-    .homepage {}
-
-    .banner {
-        position: relative;
-        width: 100%;
-        height: 221px;
-        overflow: hidden;
-        margin-bottom: 20px;
-    }
-
-    .banner-img {
-        height: 100%;
-        width: 100%;
-        object-fit: cover;
-    }
-
-    .banner-tip {
-        font-size: 14px;
-        color: white;
-        text-shadow: 0 0 1px black;
-        position: absolute;
-        bottom: 10px;
-        right: 19px;
-    }
-
     .info {
         width: 100%;
         height: 56px;
         padding: 0 20px;
+        margin-top: 20px;
         margin-bottom: 20px;
         display: flex;
         justify-content: space-between;
@@ -149,6 +197,11 @@
         white-space: nowrap;
     }
 
+
+    .footer {
+        height: 100px;
+    }
+
     .tab-0 {
         /* background-color: red; */
     }
@@ -162,4 +215,4 @@
         /* height: 10000px;  */
         /* background-color: blue; */
     }
-</style> -->
+</style>
