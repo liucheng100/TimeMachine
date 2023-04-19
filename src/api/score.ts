2bd0e2868358a3af7d69ec1008bc1620c9ef7585
SakuraKkkkk <526080338@qq.com>
2023-04-19 22:33:21 +0800

    测试

---
+++ src/api/score.ts
@@ -0,0 +1,13 @@
+import http from "@/utils/request";
+
+//(管理员)查询当前帐号未打分的作品
+export function getScoreWorks<T>(data: any) {
+    return http.get<T>({
+        url: '/score/list',
+        params: {
+            contestId: data.contestId,
+            // pageNum: data.pageNum,
+            // pageSize: data.pageSize,
+        },
+    })
+}
